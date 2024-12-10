c7a267a75f8ce9c4792b4401ecbfe69b164d5502
Liang Chen <161270933+lche0235@users.noreply.github.com>
2024-12-11 02:43:02 +1100

    add rainbow button and functionality for dynamic color changes in grid

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const container = document.getElementById('container');
 const containerSize = 400;
 const newGridBtn = document.getElementById('grid-btn');
 const resetGridBtn = document.getElementById('reset');
+const rainbowBtn = document.getElementById('rainbow');
+let rainbowMode = false
+
+rainbowBtn.addEventListener('click', rainbowFunction)
+
+function rainbowFunction() {
+    rainbowMode = !rainbowMode
+    if (rainbowMode) {
+        container.classList.add('rainbowMode') 
+        rainbowBtn.innerHTML = 'normal'
+    } else {
+        container.classList.remove('rainbowMode') 
+        rainbowBtn.innerHTML = 'rainbow'
+    }
+}
 
 newGridBtn.addEventListener('click', () => {
     const gridSize = parseInt(prompt('Enter grid size: '))
@@ -15,7 +30,7 @@ newGridBtn.addEventListener('click', () => {
 function resetGrid() {
     const gridSquares = document.querySelectorAll('div');
     gridSquares.forEach(square => {
-        square.classList.remove('hovered');
+        square.style.backgroundColor = '#94e2eb'
     })
 }
 
@@ -36,6 +51,11 @@ function createGrid(size) {
 
 function addHover(div) {
     div.addEventListener('mouseover', () => {
-        div.classList.add('hovered')
-    })
+        if (rainbowMode) {
+            const randomHue = Math.floor(Math.random() * 360);
+            div.style.backgroundColor = `hsl(${randomHue}, 100%, 50%)`;
+        } else {
+            div.style.backgroundColor = 'black';
+        }
+    });
 }
